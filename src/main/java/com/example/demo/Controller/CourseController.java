724c5e19caaa86eeeeea8d7ebe0886b45e8375af
Unknown <ckflintrup@gmail.com>
2018-05-18 14:04:16 +0200

    Get mapping indsat

--- src/main/java/com/example/demo/Controller/CourseController.java
+++ src/main/java/com/example/demo/Controller/CourseController.java
@@ -1,7 +1,24 @@
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by Ejer on 11-05-2018.
  */
+@Controller
 public class CourseController {
+
+    @GetMapping("/course")
+    public String course(){
+        return "Course";
+    }
+    @GetMapping("/createCourse")
+    public String createCourse(){
+        return "CreateCourse";
+    }
+    @GetMapping("/EditCourse")
+    public String editCourse(){
+        return "EditCourse";
+    }
 }
