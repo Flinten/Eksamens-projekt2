033b2ac1bee5d2802aa77feb6d67ee25e412d3fb
Unknown <koen_jolle@hotmail.com>
2018-05-19 12:53:03 +0200

    Auto stash before merge of "master" and "origin/master"
    adminRepo merge med getMapping og Login

--- src/main/java/com/example/demo/Controller/AdminController.java
+++ src/main/java/com/example/demo/Controller/AdminController.java
@@ -1,11 +1,20 @@
 package com.example.demo.Controller;
 
+import com.example.demo.DAO.AdminRepository;
+import com.example.demo.Utilities.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.RequestAttribute;
 /**
  * Created by Ejer on 11-05-2018.
  */
+
 @Controller
 public class AdminController {
 
@@ -13,4 +22,22 @@ public class AdminController {
     public String admin(){
         return "AdminFrontpage";
     }
+
+    @Autowired
+    AdminRepository adminRepository = new AdminRepository();
+
+    //Taget fra TG Renovation 1 års projekt af Jakob, Mikkel, Azat og Gustav
+    @PostMapping("/logIn")
+    public String logIn(@RequestParam("username") String username,
+                        @RequestParam("password") String password){
+
+        Admin adminLogin = adminRepository.logIn(username, password);
+
+        if (adminLogin.getUserName().equals(username) && adminLogin.getPassword().equals(password)){
+            return "/adminFrontPage";
+        }
+
+        return "/";
+    }
+
 }
